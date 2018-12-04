afbbe746fa9e2cf99689590b8d18455ead005a28
Nelio Alves <acenelio@gmail.com>
2018-12-04 20:52:13 -0200

    Initial implementation

---
+++ Course/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,22 @@
+﻿namespace Course.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge)
+            :base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
